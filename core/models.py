95b7cb6308924f5d9c1ec47a1f7c00157f0f8827
tuwafula <wafulaat@gmail.com>
2024-05-10 15:30:40 +0300

    add status field

--- core/models.py
+++ core/models.py
@@ -57,6 +57,7 @@ class Order(models.Model):
     customer = models.ForeignKey(User, on_delete=models.CASCADE, related_name='order')
     products = models.ManyToManyField(Product, through='OrderItem')
     name = models.CharField(max_length=255, null=True, blank=True)
+    status = models.CharField(max_length=255, default='pending')
     total_price = models.DecimalField(max_digits=100, decimal_places=2)
     shipping_address = models.TextField()
     created_at = models.DateTimeField(auto_now_add=True)
